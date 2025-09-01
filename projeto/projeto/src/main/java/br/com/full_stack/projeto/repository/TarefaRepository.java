1f89a126a87fa203063ef24fa1588cf8a745ecf0
ViNogueira <vinogueira2006@gmail.com>
2025-08-31 23:04:46 -0300

    Delete projeto/projeto directory

--- projeto/projeto/src/main/java/br/com/full_stack/projeto/repository/TarefaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.full_stack.projeto.repository;
-
-import br.com.full_stack.projeto.model.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
